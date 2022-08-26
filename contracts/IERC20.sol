de2f61a7bf0e3a93601605cef958bc76482a888d
piio-zicko <isaacijuo@gmail.com>
2022-08-26 08:59:10 +0000

    ICO Smart Contract Further Commit

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,18 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.16;
-
-
+pragma solidity ^0.8.9;
 
 interface ERC20Interface {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    function transfer(address to, uint tokens) external returns (bool success);
-    
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    function totalSupply() external view returns (uint256);
 
-        event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    function balanceOf(address tokenOwner)
+        external
+        view
+        returns (uint256 balance);
+
+    function transfer(address to, uint256 tokens)
+        external
+        returns (bool success);
+
+    function allowance(address tokenOwner, address spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    function approve(address spender, uint256 tokens)
+        external
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
 }
- 
