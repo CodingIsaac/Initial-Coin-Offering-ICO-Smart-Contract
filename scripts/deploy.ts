bd93e971f37a92aaf63725df23c34c03f509e0c8
CodingIsaac <isaacijuo@gmail.com>
2022-09-15 20:22:19 +0000

    Updated Commit

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const contract = await ethers.getContractFactory("Dabby");
+
+  const deployContract = await contract.deploy();
+
+  await deployContract.deployed();
+
+  console.log("Dabby Token Deployed Here", deployContract.address);
+
+  // Dabby Token Deployed Here 0x4458A3a6e063EAf915D7b4dbc1ae0FB524f7519f
+
+
   
 }
 
